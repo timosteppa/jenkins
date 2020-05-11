9c16bab4c66e975f65adabcc1a8408efee11dcc1
timosteppa <ec2-user@ip-192-168-0-7.us-east-2.compute.internal>
2020-05-10 19:20:25 -0700

    Made new pipeline and more mods

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
 
+import datetime
+
 print("Hello World! Tim's first repo commit")
 
 print("Fixed, jenkins not having git installed...")
 
-print("One more line, Fixed! again...")
+print("One more line, Fixed! again.... Let's see if Jenkinsfiletest actually runs")
+
+now = datetime.datetime.now()
+
+print ("Current date and time : ")
+print (now.strftime("%Y-%m-%d %H:%M:%S"))
